8c6498f525754407d4a30381e80d49169f74f97b
gulbenkorkmaz <61636741+gulbenkorkmaz@users.noreply.github.com>
2020-03-25 16:09:59 +0300

    initial commit

--- app/src/main/java/com/example/instakotlinapp/Profile/ProfileActivity.kt
+++ app/src/main/java/com/example/instakotlinapp/Profile/ProfileActivity.kt
@@ -2,6 +2,7 @@ package com.example.instakotlinapp.Profile
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.instakotlinapp.R
 import com.example.instakotlinapp.utils.BottomNavigationViewHelper
@@ -24,7 +25,19 @@ class ProfileActivity : AppCompatActivity() {
      imgProfileSettings.setOnClickListener {
      var intent=Intent(this,ProfileSettingsActivity::class.java)
          startActivity(intent)
+
      }
+         profillDüzenleButon.setOnClickListener {
+
+             profilRoot.visibility= View.GONE
+             var transaction=supportFragmentManager.beginTransaction()
+             transaction.replace(R.id.profileContainer, ProfileEditFragment())
+             transaction.addToBackStack("editFragment eklendi")
+             transaction.commit()
+
+
+         }
+
     }
 
     fun setupNavigationView() {
@@ -36,4 +49,10 @@ class ProfileActivity : AppCompatActivity() {
         var menuItem = menu.getItem(ACTIVITY_NO)
         menuItem.isChecked = true
     }
+
+    override fun onBackPressed() {
+        profilRoot.visibility= View.VISIBLE
+        super.onBackPressed()
+    }
+
 }
--- app/src/main/java/com/example/instakotlinapp/Profile/ProfileEditFragment.kt
+++ app/src/main/java/com/example/instakotlinapp/Profile/ProfileEditFragment.kt
@@ -7,18 +7,29 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.example.instakotlinapp.R
+import kotlinx.android.synthetic.main.fragment_profile_edit.view.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class ProfileEditFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_edit, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        val view =inflater.inflate(R.layout.fragment_profile_edit, container, false)
+
+        view.imgClose.setOnClickListener {
+
+            activity?.onBackPressed()
+
+
+        }
+
+
+        return view
+
+
     }
 
 }
